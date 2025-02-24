2349e101cf566d692ddb5a58ed8d6c8bf6344987
Abbey <abiodunomotoso30@gmail.com>
2025-02-24 08:32:30 +0100

    cars.js updated

--- cars.js
+++ cars.js
@@ -1,16 +1,16 @@
-// // Get the button
-// let topBtn = document.getElementById("topBtn");
+// Get the button
+let topBtn = document.getElementById("topBtn");
 
-// // Show button when scrolling down
-// window.onscroll = function () {
-//     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-//         topBtn.style.display = "block";
-//     } else {
-//         topBtn.style.display = "none";
-//     }
-// };
+// Show button when scrolling down
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
 
-// // Scroll to top when clicking the button
-// topBtn.onclick = function () {
-//     window.scrollTo({ top: 0, behavior: "smooth" });
-// };
+// Scroll to top when clicking the button
+topBtn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+};
